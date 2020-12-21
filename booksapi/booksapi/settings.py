bddac878012de8dacd51d420646b4c61f3f14db5
suma <suma@zipcodes-MacBook-Pro-6.local>
2020-12-21 15:00:24 -0500

    changes

--- booksapi/booksapi/settings.py
+++ booksapi/booksapi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,4 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+STATIC_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
+import django_on_heroku
+django_on_heroku.settings(locals())
